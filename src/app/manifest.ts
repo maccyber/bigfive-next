44c3a67c2b4334de9e21aaea20ca679f201353cc
Jonas Enge <jonas.maccyber.enge@nav.no>
2024-03-29 12:17:32 +0100

    Genesis commit

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -16,25 +16,31 @@ export default function manifest(): MetadataRoute.Manifest {
         type: 'image/x-icon',
       },
       {
-        "src": "/icon-192x192.png",
-        "sizes": "192x192",
-        "type": "image/png",
-        "purpose": "maskable"
+        src: '/icon-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+        purpose: 'maskable'
       },
       {
-        "src": "/icon-256x256.png",
-        "sizes": "256x256",
-        "type": "image/png"
+        src: '/icon-256x256.png',
+        sizes: '256x256',
+        type: 'image/png'
       },
       {
-        "src": "/icon-384x384.png",
-        "sizes": "384x384",
-        "type": "image/png"
+        src: '/icon-384x384.png',
+        sizes: '384x384',
+        type: 'image/png'
       },
       {
-        "src": "/icon-512x512.png",
-        "sizes": "512x512",
-        "type": "image/png"
+        src: '/icon-512x512.png',
+        sizes: '512x512',
+        type: 'image/png'
+      },
+      {
+        src: '/icon-144x144.png',
+        sizes: '144x144',
+        type: 'image/png',
+        purpose: 'any'
       }
     ],
   }
