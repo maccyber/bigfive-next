9ed8c967b570321049cd83475716d9df3145c5b8
Jonas Enge <jonas.maccyber.enge@nav.no>
2024-04-13 17:01:11 +0200

    Genesis commit

--- web/src/app/[locale]/compare/page.tsx
+++ web/src/app/[locale]/compare/page.tsx
@@ -2,6 +2,7 @@ import { title } from "@/components/primitives";
 import { useTranslations } from 'next-intl';
 import { ComparePeople } from "./compare-people";
 import { unstable_setRequestLocale } from 'next-intl/server';
+import { Suspense } from 'react';
 
 interface Props {
   params: { locale: string };
@@ -16,10 +17,12 @@ export default function ComparePage({ params: { locale } }: Props) {
       <br />
       <br />
       <span className="mt-2">{t('description1')}</span>
-      <ComparePeople
-        addPersonText={t('addPerson')}
-        comparePeopleText={t('comparePeople')}
-      />
+      <Suspense fallback="loading...">
+        <ComparePeople
+          addPersonText={t('addPerson')}
+          comparePeopleText={t('comparePeople')}
+        />
+      </Suspense>
     </div>
   );
 }
