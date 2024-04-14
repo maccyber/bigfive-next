0509018a8652040e74079122175f786652510f13
Jonas Enge <jonas.maccyber.enge@nav.no>
2024-04-14 22:41:11 +0200

    Genesis commit

--- web/src/app/sitemap.ts
+++ web/src/app/sitemap.ts
@@ -1,10 +1,13 @@
 import { MetadataRoute } from 'next'
 import { basePath, locales } from '@/config/site'
+import { getInfo } from '@bigfive-org/results'
 
 const articles = ['agreeableness', 'conscientiousness', 'extraversion', 'neuroticism', 'openness']
+const resultLanguages = getInfo().languages.map((l) => l.id)
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const alternatesPageLang = (path: string = '') => locales.reduce((a, v) => ({ ...a, [v]: basePath + `/${v}${path}` }), {})
+  const alternatesParamsLang = (path: string = '') => resultLanguages.reduce((a, v) => ({ ...a, [v]: basePath + `${path}?lang=${v}` }), {})
   return [
     {
       url: basePath,
@@ -23,12 +26,16 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: `${basePath}/result/58a70606a835c400c8b38e84`,
       lastModified: new Date(),
-      // add lang
+      alternates: {
+        languages: alternatesParamsLang('/result/58a70606a835c400c8b38e84')
+      }
     },
     {
       url: `${basePath}/compare/W3sibmFtZSI6Ik1hcnZpbiIsImlkIjoiNThhNzA2MDZhODM1YzQwMGM4YjM4ZTg0In0seyJuYW1lIjoiQXJ0aHVyIERlbnQiLCJpZCI6IjVlNTZiYTdhYjA5NjEzMDAwN2Q1ZDZkOCJ9LHsibmFtZSI6IkZvcmQgUGVyZmVjdCIsImlkIjoiNWRlYTllODhlMTA4Y2IwMDYyMTgzYWYzIn0seyJuYW1lIjoiU2xhcnRpYmFydGZhc3QiLCJpZCI6IjVlNTZiNjUwYjA5NjEzMDAwN2Q1ZDZkMCJ9XQ==`,
       lastModified: new Date(),
-      // add lang
+      alternates: {
+        languages: alternatesPageLang('/compare/W3sibmFtZSI6Ik1hcnZpbiIsImlkIjoiNThhNzA2MDZhODM1YzQwMGM4YjM4ZTg0In0seyJuYW1lIjoiQXJ0aHVyIERlbnQiLCJpZCI6IjVlNTZiYTdhYjA5NjEzMDAwN2Q1ZDZkOCJ9LHsibmFtZSI6IkZvcmQgUGVyZmVjdCIsImlkIjoiNWRlYTllODhlMTA4Y2IwMDYyMTgzYWYzIn0seyJuYW1lIjoiU2xhcnRpYmFydGZhc3QiLCJpZCI6IjVlNTZiNjUwYjA5NjEzMDAwN2Q1ZDZkMCJ9XQ==')
+      }
     },
     {
       url: `${basePath}/test`,
