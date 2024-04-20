0c0461254da30de3d2cd5eeae3446af4071fe95a
Jonas Enge <jonas.maccyber.enge@nav.no>
2024-04-20 21:44:45 +0200

    Adds turkish and ipipId

---
+++ old-packages/questions/data/tr/choices.js
@@ -0,0 +1,56 @@
+module.exports = {
+  plus: [
+    {
+      text: 'Çok yanlış',
+      score: 1,
+      color: 1
+    },
+    {
+      text: 'Orta Derecede Yanlış',
+      score: 2,
+      color: 2
+    },
+    {
+      text: 'Ne Doğru Ne Yanlış',
+      score: 3,
+      color: 3
+    },
+    {
+      text: 'Orta Derecede Doğru',
+      score: 4,
+      color: 4
+    },
+    {
+      text: 'Çok doğru',
+      score: 5,
+      color: 5
+    }
+  ],
+  minus: [
+    {
+      text: 'Çok yanlış',
+      score: 5,
+      color: 1
+    },
+    {
+      text: 'Orta Derecede Yanlış',
+      score: 4,
+      color: 2
+    },
+    {
+      text: 'Ne Doğru Ne Yanlış',
+      score: 3,
+      color: 3
+    },
+    {
+      text: 'Orta Derecede Doğru',
+      score: 2,
+      color: 4
+    },
+    {
+      text: 'Çok doğru',
+      score: 1,
+      color: 5
+    }
+  ]
+}
